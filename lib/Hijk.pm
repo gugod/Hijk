766225d714a011e6780df9201b05628f39259bfd
Kang-min Liu <gugod@gugod.org>
2013-12-03 23:36:52 +0100

    bump version and noted the major change.

--- lib/Hijk.pm
+++ lib/Hijk.pm
@@ -5,7 +5,7 @@ use warnings;
 use POSIX qw(EINPROGRESS);
 use Socket qw(PF_INET SOCK_STREAM sockaddr_in inet_aton $CRLF);
 use Fcntl qw(F_GETFL F_SETFL O_NONBLOCK);
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 my $SocketCache = {};
 
 eval {
