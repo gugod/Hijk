147fcfa0d2e43709e5cf0c1bd90a9c97122d3430
borislav nikolov <jack@sofialondonmoskva.com>
2013-12-06 21:05:23 +0100

    bench-elasticsearch: use the new fetch => \&Hijk::HTTP::XS::fetch to benchmark; update the results

--- examples/bench-elasticsearch.pl
+++ examples/bench-elasticsearch.pl
@@ -3,13 +3,15 @@ use strict;
 use warnings;
 use Benchmark;
 use Hijk;
+use Hijk::HTTP::XS;
 use HTTP::Tiny;
 use LWP::UserAgent;
 use HTTP::Request;
 
 my $tiny = HTTP::Tiny->new();
 my $req = HTTP::Request->new('GET','http://localhost:9200/_search');
-$req->content(q!{"query":{"match_all":{}}}!);
+my $body = '{"query":{"match_all":{}}}';
+$req->content($body);
 my $lwp = LWP::UserAgent->new();
 
 # run with HACKED_REQUEST_PP=1 perl bench-elasticsearch.pl
@@ -17,33 +19,31 @@ my $lwp = LWP::UserAgent->new();
 # current results on Intel(R) Core(TM)2 Duo CPU P8400@2.26GHz with 2gb ram
 # and elasticsearch with one index containing ~ 500 small documents:
 
-# Benchmark: timing 100000 iterations of hijk pp, hijk xs, lwp____, tiny___...
-#   hijk pp: 34 wallclock secs ( 6.45 usr +  3.02 sys =  9.47 CPU) @ 10559.66/s (n=100000)
-#   hijk xs: 32 wallclock secs ( 4.38 usr +  3.38 sys =  7.76 CPU) @ 12886.60/s (n=100000)
-#   lwp____: 200 wallclock secs (147.66 usr + 18.68 sys = 166.34 CPU) @ 601.18/s (n=100000)
-#   tiny___: 112 wallclock secs (61.56 usr + 21.15 sys = 82.71 CPU) @ 1209.04/s (n=100000)
+#Benchmark: timing 10000 iterations of hijk pp, hijk xs, lwp____, tiny___...
+#   hijk pp:  4 wallclock secs ( 0.79 usr +  0.20 sys =  0.99 CPU) @ 10101.01/s (n=10000)
+#   hijk xs:  3 wallclock secs ( 0.48 usr +  0.30 sys =  0.78 CPU) @ 12820.51/s (n=10000)
+#   lwp____: 21 wallclock secs (15.06 usr +  1.91 sys = 16.97 CPU) @ 589.28/s (n=10000)
+#   tiny___: 11 wallclock secs ( 6.22 usr +  2.14 sys =  8.36 CPU) @ 1196.17/s (n=10000)
 
 
-timethese(1_00_000,{
+timethese(10_000,{
     'tiny___' => sub {
-        my $res = $tiny->get('http://localhost:9200/_search',{content => '{"query":{"match_all":{}}}' });
+        my $res = $tiny->get('http://localhost:9200/_search',{content => $body });
     },
     'hijk xs' => sub {
-        my $res = Hijk::request({path => "/_search", body => q!{"query":{"match_all":{}}}!,
+        my $res = Hijk::request({path => "/_search", body => $body,
+                                 host => 'localhost',
+                                 port => 9200,
+                                 fetch => \&Hijk::HTTP::XS::fetch,
+                                 method => 'GET'});
+    },
+    'hijk pp' => sub {
+        my $res = Hijk::request({path => "/_search", body => $body,
                                  host => 'localhost',
                                  port => 9200,
                                  method => 'GET'});
     },
-    ($ENV{HACKED_REQUEST_PP} ? (
-    'hijk pp' => sub {
-        my $res = Hijk::request_pp({path => "/_search", body => q!{"query":{"match_all":{}}}!,
-                                 host => 'localhost',
-                                 port => 9200,
-                                 method => 'GET'});
-    }
-    ) : ()),
     'lwp____' => sub {
         my $res = $lwp->request($req);
     },
-
 });
