1fd4218974d29c833e3a4497d27004798b68661d
borislav nikolov <jack@sofialondonmoskva.com>
2014-07-05 14:17:00 +0200

    add chunked encoding support; correctly handle no content len header
    * we were assuming that there is nothing to read with no content len
    header, but in fact that is a valid 1.1 state, in which case we must
    read until the connection is closed. The problem is very ugly when
    socket cache is used, because the next read will actually read a left
    over data from the no-content-len request

--- lib/Hijk.pm
+++ lib/Hijk.pm
@@ -22,14 +22,15 @@ sub read_http_message {
     $read_timeout = selectable_timeout($read_timeout);
     my ($body,$buf,$decapitated,$nbytes,$proto);
     my $status_code = 0;
+    my $no_content_len = 0;
     vec(my $rin = '', $fd, 1) = 1;
     do {
         my $nfound = select($rin, undef, undef, $read_timeout);
-
         return (undef,0,undef,undef, Hijk::Error::READ_TIMEOUT)
             if ($nfound != 1 || (defined($read_timeout) && $read_timeout <= 0));
 
         my $nbytes = POSIX::read($fd, $buf, $block_size);
+        goto end if $no_content_len && $decapitated && (!defined($nbytes) || $nbytes == 0);
         if (!defined($nbytes)) {
             next
                 if ($! == EWOULDBLOCK || $! == EAGAIN);
@@ -41,7 +42,9 @@ sub read_http_message {
 
         if ($decapitated) {
             $body .= $buf;
-            $block_size -= $nbytes;
+            if (!$no_content_len) {
+                $block_size -= $nbytes;
+            }
         }
         else {
             $head .= $buf;
@@ -59,18 +62,68 @@ sub read_http_message {
                     $header->{$key} = $value;
                 }
 
+                if ($header->{'Transfer-Encoding'} && $header->{'Transfer-Encoding'} eq 'chunked') {
+                    # if there is chunked encoding we have to ignore content lenght even if we have it
+                    return ($proto, $status_code, read_chunked_body($body, $fd, $read_timeout), $header);
+                }
+
                 if ($header->{'Content-Length'}) {
                     $block_size = $header->{'Content-Length'} - length($body);
                 }
                 else {
-                    $block_size = 0;
+                    $block_size = 10204;
+                    $no_content_len = 1;
                 }
             }
         }
 
-    } while( !$decapitated || $block_size > 0 );
-
+    } while( !$decapitated || $block_size > 0 || $no_content_len);
+  end:
     return ($proto, $status_code, $body, $header);
+}
+
+sub read_chunked_body {
+    my ($buf,$fd, $read_timeout) = @_;
+    my $chunk_size   = 0;
+    my $body         = "";
+    my $block_size = 10240;
+    vec(my $rin = '', $fd, 1) = 1;
+
+    while(1) {
+        # just read a 10k block and process it until it is consumed
+        if (length($buf) == 0 || length($buf) < $chunk_size) {
+            my $nfound = select($rin, undef, undef, $read_timeout);
+            return (undef,0,undef,undef, Hijk::Error::READ_TIMEOUT)
+                if ($nfound != 1 || (defined($read_timeout) && $read_timeout <= 0));
+            my $current_buf = "";
+            my $nbytes = POSIX::read($fd, $current_buf, $block_size);
+            if (!defined($nbytes)) {
+                next
+                    if ($! == EWOULDBLOCK || $! == EAGAIN);
+                die "Failed to read chunked body from socket. errno = $!"
+            }
+
+            die "Failed to read chunked body from socket. Got 0 bytes back, which shouldn't happen <$buf> <$current_buf>"
+                if $nbytes == 0;
+            $buf .= $current_buf;
+        }
+
+        if ($chunk_size > 0 && length($buf) >= $chunk_size) {
+            $body .= substr($buf, 0, $chunk_size - 2); # our chunk size includes the CRLF
+            $buf = substr($buf, $chunk_size);
+            $chunk_size = 0;
+        } else {
+            my $neck_pos = index($buf, ${CRLF});
+            if ($neck_pos > 0) {
+                $chunk_size = hex(substr($buf, 0, $neck_pos));
+                if ($chunk_size == 0) {
+                    return $body;
+                }
+                $chunk_size += 2;                      # include the final CRLF
+                $buf = substr($buf, $neck_pos + 2);
+            }
+        }
+    }
 }
 
 sub construct_socket {
---
+++ t/chunked.t
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+use strict;
+use Test::More;
+use File::Temp ();
+use File::Temp qw/ :seekable /;
+use Hijk;
+use Test::Exception;
+
+my $fh = File::Temp->new();
+my $fd = do {
+    local $/ = undef;
+    my $data = "4\r\nWiki\r\n5\r\npedia\r\ne\r\n in\r\n\r\nchunks.\r\n0\r\n\r\n";
+
+    my $msg = join(
+        "\x0d\x0a",
+        'HTTP/1.1 200 OK',
+        'Date: Sat, 23 Nov 2013 23:10:28 GMT',
+        'Last-Modified: Sat, 26 Oct 2013 19:41:47 GMT',
+        'ETag: "4b9d0211dd8a2819866bccff777af225"',
+        'Content-Type: text/html',
+        'Server: Example',
+        'Transfer-Encoding: chunked',
+        '',
+        $data
+    );
+    print $fh $msg;
+    $fh->flush;
+    $fh->seek(0, 0);
+    fileno($fh);
+};
+
+my ($proto, $status, $body, $head) = Hijk::read_http_message($fd);
+
+
+is $status, 200;
+is $body, "Wikipedia in\r\n\r\nchunks.";
+
+is_deeply $head, {
+    "Date" => "Sat, 23 Nov 2013 23:10:28 GMT",
+    "Last-Modified" => "Sat, 26 Oct 2013 19:41:47 GMT",
+    "ETag" => '"4b9d0211dd8a2819866bccff777af225"',
+    "Content-Type" => "text/html",
+    "Server" => "Example",
+    "Transfer-Encoding" => "chunked",
+};
+
+# fetch again without seeking back
+# this will force select() to return because there are actually
+# 0 bytes to read - so we can simulate connection closed 
+# from the other end of the socket (like expired keep-alive)
+throws_ok {
+    my ($proto, $status, $body, $head) = Hijk::read_http_message($fd);
+} qr /0 bytes/i;
+
+done_testing;
